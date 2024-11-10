798871e27ebde7e259ae8bd72fdd48744ee746aa
Jakob <jakobyl.steffens@gmail.com>
2024-11-10 01:24:00 +0100

    Url Encode on search

--- SpotS/Search.cpp
+++ SpotS/Search.cpp
@@ -13,7 +13,7 @@
 // Returns:
 // - A JSON string containing the search results if successful; otherwise, an error string.
 std::string Spotify::_Search::search(const std::string& q, const std::vector<const char*>& types, const int limit, const int offset, const std::string& market) const {
-	std::string url = "https://api.spotify.com/v1/search?q=" + q + "&limit=" + std::to_string(limit) + "&offset=" + std::to_string(offset) + (market.empty() ? "" : "&market=" + market);
+	std::string url = "https://api.spotify.com/v1/search?q=" + Net::url_encode(q) + "&limit=" + std::to_string(limit) + "&offset=" + std::to_string(offset) + (market.empty() ? "" : "&market=" + market);
 
 	// Append each type to the URL
 	url += "&type=";
